75944b02da2196d5c4a2fdd387eaf964ab0ffad0
paulacarvani <3312@holbertonschool.com>
2021-09-22 17:13:28 -0500

    Update Header File

--- monty.h
+++ monty.h
@@ -1,9 +1,11 @@
-#ifndef MONTY_HH
-#define MONTY_HH
+#ifndef MONTY_H
+#define MONTY_H
 
+#define  _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -12,30 +14,41 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* Prototypes here */
-
+int take_num;
+int get_opcode(char *token, unsigned int line, stack_t **stack);
+void _pint(stack_t **top, unsigned int line_number);
+void _pall(stack_t **top, unsigned int line_number);
+void _push(stack_t **top, unsigned int line_number);
+int is_number(char *token);
+int open_file_to_read(char *filename, stack_t **stack);
+void _free(char *buff_line, FILE *in_file);
+void free_stack(stack_t **stack);
+void _pop(stack_t **top, unsigned int line_number);
+void _swap(stack_t **top, unsigned int line_number);
+void _add(stack_t **top, unsigned int line_number);
+void _nop(stack_t **top, unsigned int line_number);
+void _sub(stack_t **top, unsigned int line_number);
 
 #endif
