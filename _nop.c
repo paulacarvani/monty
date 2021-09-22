d30cd4be33f117ad1ec51cceccd35864d655cc20
paulacarvani <3312@holbertonschool.com>
2021-09-22 17:33:27 -0500

    Task 5

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - Function that doesnt do anything
+ * @top: element at the top of the stack (head)
+ * @line_number: constant int value in the structure
+ * Return: void
+ **/
+void _nop(stack_t **top, unsigned int line_number)
+{
+	(void) top;
+	(void) line_number;
+}
