791abd453a2a60b7ea2c0c62237fa28153a14215
paulacarvani <3312@holbertonschool.com>
2021-09-22 17:44:03 -0500

    Task 9 Advanced

---
+++ 9_mod.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ *_mod - Module of the second top element of the stack by the top element
+ * @top: element at the top of the stack (head)
+ * @line_number: constant int value in the structure
+ * Return: void
+ */
+void _mod(stack_t **top, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (*top == NULL || (*top)->next ==  NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*top)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*top)->next;
+	tmp->n = (tmp->n) % (*top)->n;
+	_pop(top, line_number);
+}
